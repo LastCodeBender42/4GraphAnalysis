9f762bc16c2736c1a148d9903cb9a1fe46f315a2
foutchda <d.foutch@vumc.org>
2024-08-16 16:25:23 -0500

    update init file

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
+"""
+4GraphAnalysis Plugin for PyMOL
+Author: David Foutch
+Contact: dfoutch@analysisandinformatics.org
+Version: 1.0
+Description: A PyMOL plugin for graph analysis of protein structures.
+"""
+
 from pymol.plugins import addmenuitemqt
 from .my_plugin import get_protein_name  # Import get_protein_name from my_plugin.py
 from . import main_window  # Import main_window from the current package
